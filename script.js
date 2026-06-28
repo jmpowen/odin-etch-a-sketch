a478eb1ff824c30c84fe3c1b78be4749f69f87f2
unknown <powen.jack@gmail.com>
2026-06-27 19:50:36 -0500

    added changing colors to pixel-divs

--- script.js
+++ script.js
@@ -15,6 +15,14 @@ RESET_BTN.addEventListener('click', () => {
   createGrid(x, y);
 })
 
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
 function createGrid(hori, vert) {
   const MAX_PIXELS = 1000;
   for (let i = 0; i < vert; i++) {
@@ -24,18 +32,19 @@ function createGrid(hori, vert) {
     DIV_1.className = "grid-row";
     for (let j = 0; j < hori; j++) {
       const DIV_2 = document.createElement('div');
-      DIV_2.style.background = "black";
       DIV_2.style.height = Math.floor(MAX_PIXELS/hori) + "px";
       DIV_2.style.width = Math.floor(MAX_PIXELS/vert) + "px";
+      DIV_2.style.backgroundColor = "#FFFFFF";
       DIV_2.className = "pixel-div";
 
       DIV_2.addEventListener('mouseenter', () => {
-        DIV_2.style.backgroundColor = '#2ecc71';
+        DIV_2.style.backgroundColor = getRandomColor();
       });
 
       // Revert color when mouse leaves
       DIV_2.addEventListener('mouseleave', () => {
-        DIV_2.style.backgroundColor = "black"; // Reverts to original CSS
+        DIV_2.style.backgroundColor = 'white';
+      
       });
 
       DIV_1.appendChild(DIV_2);
