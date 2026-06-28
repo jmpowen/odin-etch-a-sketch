7619bd90d0d2ad94814103d817d28d8d6b0237cf
unknown <powen.jack@gmail.com>
2026-06-28 01:09:12 -0500

    minor fiexes

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ const GRID_DIV = document.querySelector('.grid');
 const RESET_BTN = document.querySelector('.reset-btn');
 
 
-const MAX_PIXELS = 1000;
+const MAX_PIXELS = 600;
 let x = 16;
 let y = 16;
 
 RESET_BTN.addEventListener('click', () => {
   do {
     x = prompt("What length horizontally should the grid be?");
-  } while (!Number.isInteger(Number(x)) || Number(x) > 100)
+  } while (!Number.isInteger(Number(x)) || Number(x) > 100 || Number(x) < 1 || x === "")
   do {
     y = prompt("What length vertically should the grid be?");
-  } while (!Number.isInteger(Number(y)) || Number(y) > 100)
+  } while (!Number.isInteger(Number(y)) || Number(y) > 100 || Number(x) < 1 || x === "")
 
   document.querySelectorAll('.grid-row').forEach(div => div.remove());
   createGrid(x, y);
@@ -35,15 +35,16 @@ function createGrid(hori, vert) {
     DIV_1.className = "grid-row";
     for (let j = 0; j < hori; j++) {
       const DIV_2 = document.createElement('div');
-      DIV_2.style.height = Math.floor(MAX_PIXELS/hori) + "px";
-      DIV_2.style.width = Math.floor(MAX_PIXELS/vert) + "px";
+      DIV_2.style.height = Math.floor(MAX_PIXELS/vert) + "px";
+      DIV_2.style.width = Math.floor(MAX_PIXELS/hori) + "px";
       DIV_2.style.backgroundColor = "#FFFFFF";
-      DIV_2.className = "pixel-div";
+      DIV_2.style.border = "1px solid black";
 
       DIV_2.addEventListener('mouseenter', () => {
         DIV_2.style.backgroundColor = getRandomColor();
         DIV_2.style.opacity = 1;
         DIV_2.style.transition = "";
+        DIV_2.style.border = "1px solid black";
       });
 
       // Revert color when mouse leaves
